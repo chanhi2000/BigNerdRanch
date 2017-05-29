9a4502b101cbf421a2b402ac97d42dcf638f267b
chanhi2000 <chanhi2000@gmail.com>
2017-05-29 16:01:03 +0900

    chapter01 added

---
+++ chapter01/RandomPassword/RandomPassword/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  RandomPassword
+//
+//  Created by LeeChan on 5/29/17.
+//  Copyright © 2017 MarkiiimarK. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
